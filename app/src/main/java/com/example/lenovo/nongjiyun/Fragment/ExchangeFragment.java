cd434cdcc44919dd437432e42fa4529c4871eb09
wzx <609247863@qq.com>
2018-05-03 16:51:07 +0800

    ghjhhjg

--- app/src/main/java/com/example/lenovo/nongjiyun/Fragment/ExchangeFragment.java
+++ app/src/main/java/com/example/lenovo/nongjiyun/Fragment/ExchangeFragment.java
@@ -19,7 +19,7 @@ public class ExchangeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
